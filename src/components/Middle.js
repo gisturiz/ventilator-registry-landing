78f4fb243f58edf995c0846a368d4de900a4c709
Gustavo Isturiz <isturizgustavo@gmail.com>
2020-09-16 11:58:47 -0400

    middle section complete

--- src/components/Middle.js
+++ src/components/Middle.js
@@ -8,15 +8,18 @@ export default function Middle() {
         <div className="middle_container">
             <div className="container">
                 <h2>Geolocation</h2>
-                <img src={Pin} alt="map pin" style={{ 'width': '35%' }} />
+                <img src={Pin} alt="map pin" style={{ 'height': '70px' }} />
+                <p>Easily view your network's ventilator <br/>supply on an interactive map.</p>
             </div>
             <div className="container">
                 <h2>Real-Time Data</h2>
-                <img src={Flash} alt="lighting bolt" style={{ 'width': '35%', 'color': 'blue' }} />
+                <img src={Flash} alt="lighting bolt" style={{ 'height': '70px'}} />
+                <p>Changes in the registry are updated in real-time <br/> so you have the most current information.</p>
             </div>
             <div className="container">
                 <h2>Analytic Tools</h2>
-                <img src={Analytics} alt="computer analysis" style={{ 'width': '35%' }} />
+                <img src={Analytics} alt="computer analysis" style={{ 'height': '70px' }} />
+                <p>Search, filter and export data so you can <br/> better analyze your network's data.</p>
             </div>
         </div>
     )
